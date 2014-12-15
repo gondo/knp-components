93eb1f0a060e101ddf2eb05a18fadf1fe3b390e7
gondo <gondo@webdesigners.sk>
2014-12-15 13:50:32 +0000

    sync with original

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,50 +1,14 @@
 <?php
-
-/**
- * This is bootstrap for phpUnit unit tests,
- * use README.md for more details
- */
-
-if (!class_exists('PHPUnit_Framework_TestCase') ||
-    version_compare(PHPUnit_Runner_Version::id(), '3.5') < 0
-) {
-    die('PHPUnit framework is required, at least 3.5 version');
+function includeIfExists($file)
+{
+    if (file_exists($file)) {
+        return include $file;
+    }
 }
-
-if (!class_exists('PHPUnit_Framework_MockObject_MockBuilder')) {
-    die('PHPUnit MockObject plugin is required, at least 1.0.8 version');
+if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../.composer/autoload.php'))) {
+    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL);
 }
-
-define('TESTS_PATH', __DIR__);
-define('VENDOR_PATH', realpath(__DIR__ . '/../vendor'));
-
-$classLoaderFile = VENDOR_PATH . '/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-if (!file_exists($classLoaderFile)) {
-    die('cannot find vendor, run: php bin/vendors.php');
-}
-require_once $classLoaderFile;
-$loader = new Symfony\Component\ClassLoader\UniversalClassLoader;
-$loader->registerNamespaces(array(
-    'Symfony'                    => VENDOR_PATH,
-    'Knp'                        => __DIR__.'/../src',
-    'Test'                       => __DIR__,
-    'Doctrine\\Common'           => VENDOR_PATH.'/doctrine-common/lib',
-    'Doctrine\\DBAL'             => VENDOR_PATH.'/doctrine-dbal/lib',
-    'Doctrine\\ORM'              => VENDOR_PATH.'/doctrine-orm/lib',
-    'Doctrine\\MongoDB'          => VENDOR_PATH.'/doctrine-mongodb/lib',
-    'Doctrine\\ODM\\MongoDB'     => VENDOR_PATH.'/doctrine-mongodb-odm/lib',
-));
-$loader->register();
-
-\Doctrine\Common\Annotations\AnnotationRegistry::registerFile(
-    VENDOR_PATH.'/doctrine-orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php'
-);
-
-\Doctrine\Common\Annotations\AnnotationRegistry::registerFile(
-    VENDOR_PATH.'/doctrine-mongodb-odm/lib/Doctrine/ODM/MongoDB/Mapping/Annotations/DoctrineAnnotations.php'
-);
-
-$reader = new \Doctrine\Common\Annotations\AnnotationReader();
-$reader = new \Doctrine\Common\Annotations\CachedReader($reader, new \Doctrine\Common\Cache\ArrayCache());
-$_ENV['annotation_reader'] = $reader;
-
+$loader->add('Knp\Bundle\PaginatorBundle\Tests', __DIR__);
+return $loader;
